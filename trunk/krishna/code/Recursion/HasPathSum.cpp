0f3aa53536e8745da10055e39d688a59a8bf3974
chander.krishna@gmail.com <chander.krishna@gmail.com@fd763d81-938d-ea44-d03e-e3a211f734d2>
2012-08-16 15:27:53 +0000

    fix: check node value - current value is eaual to zero when checking for current node's children are null

--- trunk/krishna/code/Recursion/HasPathSum.cpp
+++ trunk/krishna/code/Recursion/HasPathSum.cpp
@@ -6,9 +6,9 @@ int main(int argc, char* argv[])
 
 	HasPathSum<int>* path = new HasPathSum<int>();
 
-	bool ret = path->FindPathSum(head, 7);
+	bool ret = path->FindPathSum(head, 9);
 
-	std::cout << "Has Path Suma: " << ret;
+	std::cout << "Has Path Suma: " << ret ? "true" : "false";
 
 	getchar();
 }
--- trunk/krishna/code/Recursion/HasPathSum.h
+++ trunk/krishna/code/Recursion/HasPathSum.h
@@ -28,7 +28,7 @@ bool HasPathSum<T>::FindPathSum(Node<T>* node, T value)
 		return false;
 
 	// the tree has path sum, so return true
-	if(node != nullptr && node->left == nullptr && node->right == nullptr && value == 0)
+	if(node != nullptr && node->left == nullptr && node->right == nullptr && (node->item - value) == 0)
 		return true;
 
 	T currentValue = value - node->item;
